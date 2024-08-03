35dc8b0766ee67027dc59004557c2fde763fe927
danilonascimento0 <dbnascimentto@gmail.com>
2024-08-03 11:09:50 -0300

    First commit

---
+++ src/main/java/com/danilo/notification_service/NotificationApp.java
@@ -0,0 +1,13 @@
+package com.danilo.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilo/notification_service/NotificationAppTests.java
@@ -0,0 +1,13 @@
+package com.danilo.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
