10d6e86bdf303c972732047510d710ea574d6c88
danilonascimento0 <dbnascimentto@gmail.com>
2024-08-03 17:50:37 -0300

    Change notifications strategy

--- src/main/java/com/danilo/notification_service/service/NotificationService.java
+++ src/main/java/com/danilo/notification_service/service/NotificationService.java
@@ -4,7 +4,12 @@ import com.danilo.notification_service.model.MessageDTO;
 import com.danilo.notification_service.model.enums.Channel;
 import com.danilo.notification_service.model.notification.EmailNotification;
 import com.danilo.notification_service.model.notification.Notification;
+import com.danilo.notification_service.model.notification.PushNotification;
+import com.danilo.notification_service.model.notification.SMSNotification;
 import com.danilo.notification_service.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationService {
 
@@ -16,15 +21,26 @@ public class NotificationService {
 
 	public void sendNotification(MessageDTO messageDTO) {
 
+		List<Notification> notificationList = new ArrayList<>();
+
 		this.userRepository.findBySubscription(messageDTO.getSubscription()).forEach(user -> {
 			user.getChannelList().forEach(channel -> {
 				if (channel.equals(Channel.EMAIL)) {
-					Notification notification = new EmailNotification(messageDTO.getMessage(), messageDTO.getSubscription(), user);
-					notification.send();
+					notificationList.add(
+							new EmailNotification(messageDTO.getMessage(), messageDTO.getSubscription(), user));
+				}
+				if (channel.equals(Channel.SMS)) {
+					notificationList.add(
+							new SMSNotification(messageDTO.getMessage(), messageDTO.getSubscription(), user));
+				}
+				if (channel.equals(Channel.PUSH_NOTIFICATION)) {
+					notificationList.add(
+							new PushNotification(messageDTO.getMessage(), messageDTO.getSubscription(), user));
 				}
 			});
 		});
 
+		notificationList.forEach(Notification::send);
 	}
 
 }
